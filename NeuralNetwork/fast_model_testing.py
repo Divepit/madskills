c86df8ac23cffdec03d66d193f3c482cffce400a
marcotr <marcotr@ethz.ch>
2024-11-02 14:03:46 +0100

    refactor

--- NeuralNetwork/fast_model_testing.py
+++ NeuralNetwork/fast_model_testing.py
@@ -10,40 +10,24 @@ from planning_sandbox.visualizer_class import Visualizer
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# @keras.saving.register_keras_serializable()
-# def custom_mae_with_rounded_zero_penalty(y_true, y_pred):
-#     mae_loss = tf.reduce_mean(tf.abs(y_true - y_pred))
-#     rounded_pred = tf.round(y_pred)
-#     rounded_zero_mask = tf.cast(tf.equal(rounded_pred, 0), tf.float32)
-#     non_zero_true_mask = tf.cast(tf.not_equal(y_true, 0), tf.float32)
-#     penalty_mask = rounded_zero_mask * non_zero_true_mask
-#     penalty = tf.reduce_sum(penalty_mask)
-#     loss = mae_loss + 0.5 * penalty
-#     return loss
+
 
 @keras.saving.register_keras_serializable()
 def rounded_accuracy(y_true, y_pred):
-    # Round the predictions to the nearest integer
     y_pred_rounded = tf.round(y_pred)
-    
-    # Check if the rounded predictions match the true values
     correct_predictions = tf.cast(tf.equal(y_true, y_pred_rounded), dtype=tf.float32)
-    
-    # Calculate the mean accuracy
     return tf.reduce_mean(correct_predictions)
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
 model = keras.models.load_model(current_directory+'/models/good_3a_5g_2sk_100x100_27min.keras')
 # model = keras.models.load_model(current_directory+'/model.keras')
 
-# Parameters (should match data generation parameters)
 num_agents = 3
 num_goals = 5
 num_skills = 2
 size = 200
 use_geo_data = True
 
-# Create environment (needed for input generation and visualization)
 env = Environment(size=size, num_agents=num_agents, num_goals=num_goals,
                   num_skills=num_skills, use_geo_data=use_geo_data)
 vis = Visualizer(env, speed=50)
